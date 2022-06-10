042d8b65d7c50715ae1579604c5fc691a72fbf31
Redmist-bit <bayuadeirawan79@gmail.com>
2022-06-10 09:08:55 +0700

    done

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let IXX = await promiseTheaterIXX()
+  let VGC = await promiseTheaterVGC()
+  return new Promise((resolve, reject) => {
+    if (emosi === null) {
+      reject('Parameter tidak ada');
+    }
+    else {
+      resolve(IXX.concat(VGC).filter(e => e.hasil === emosi).length)
+    }
+  });
+};
 
 module.exports = {
   promiseOutput,
